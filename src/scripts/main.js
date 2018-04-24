94adbd6fb5f8b41f3e13cfef23e917210c2e0189
Koos Bavinck <>
2018-04-24 23:14:14 +0200

    sticky nav javascript (backup)

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+// const nav = document.querySelector('#fixedNav')
+// const subjectHeader = document.querySelector('#vakbeschrijving')
+
+// window.addEventListener('scroll', () => {
+//   let navBounds = nav.getBoundingClientRect()
+//   let headerBounds = subjectHeader.getBoundingClientRect()
+//   if (headerBounds.top < 64) {
+//     nav.style.cssText = `position: fixed; left: ${navBounds.left}px; top: 2rem; width: ${navBounds.width}px`
+//   } else {
+//     nav.style.cssText = ''
+//   }
+// })
