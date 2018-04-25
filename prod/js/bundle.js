aa58d6948d3f4d1771558da7075a187bc536b65c
Koos Bavinck <>
2018-04-25 16:39:16 +0200

    update pages

--- prod/js/bundle.js
+++ prod/js/bundle.js
@@ -1,1 +1,18 @@
-"use strict";var radioGroups=document.querySelectorAll(".radio"),userHTML=document.createElement("div");userHTML.classList.add("user"),userHTML.innerHTML='<img src="images/users/profile.jpg"><div class="hidden"><img src="images/users/profile.jpg"> Koos Bavinck</div>',radioGroups.forEach(function(e){var s=e.querySelector(".users"),r=e.querySelector("input");r.addEventListener("change",function(){r.checked&&(s.appendChild(userHTML),userHTML.classList.remove("newUser"),userHTML.classList.add("newUser"))})});
+'use strict';
+
+var radioGroups = document.querySelectorAll('.radio');
+var userHTML = document.createElement('div');
+userHTML.classList.add('user');
+userHTML.innerHTML = '<img src="images/users/profile.jpg"><div class="hidden"><img src="images/users/profile.jpg"> Koos Bavinck</div>';
+
+radioGroups.forEach(function (radio) {
+  var users = radio.querySelector('.users');
+  var input = radio.querySelector('input');
+  input.addEventListener('change', function () {
+    if (input.checked) {
+      users.appendChild(userHTML);
+      userHTML.classList.remove('newUser');
+      userHTML.classList.add('newUser');
+    }
+  });
+});
