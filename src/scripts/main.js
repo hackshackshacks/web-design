f662a87f5aaab0dfa5eca5b2e03003159ab1170b
Koos Bavinck <>
2018-04-25 14:59:25 +0200

    fix focus states

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,21 @@
 //     nav.style.cssText = ''
 //   }
 // })
+const radioGroups = document.querySelectorAll('.radio')
+const userHTML = document.createElement('div')
+userHTML.classList.add('user')
+userHTML.innerHTML = '<img src="images/users/profile.jpg"><div class="hidden"><img src="images/users/profile.jpg"> Koos Bavinck</div>'
+
+radioGroups.forEach((radio) => {
+  const users = radio.querySelector('.users')
+  const input = radio.querySelector('input')
+  input.addEventListener('change', () => {
+    if (input.checked) {
+      console.log('test')
+      users.appendChild(userHTML)
+    } else {
+      users.insertAdjacentHTML('beforeend', 'test')
+    }
+  })
+})
+
